50e41190fa04de89d4e340d0a3ddd26e48342d3b
Dinesh Kumar <dk9671399889@gmail.com>
2023-08-28 13:07:05 +0530

    Updates

---
+++ precache-manifest.3afa0756306b1e6fc7d266fb29ae72b9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c843fc81e219ee19b8fd",
+    "url": "/react-portfolio/static/css/main.9d8c40d4.chunk.css"
+  },
+  {
+    "revision": "c843fc81e219ee19b8fd",
+    "url": "/react-portfolio/static/js/main.c843fc81.chunk.js"
+  },
+  {
+    "revision": "7efd5ba37217da21e3ad",
+    "url": "/react-portfolio/static/js/1.7efd5ba3.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "60505afe12f2491c28233fb3cba35b7f",
+    "url": "/react-portfolio/static/media/logo.60505afe.png"
+  },
+  {
+    "revision": "a82f274d7197640f7b935dcfe8b8f4f1",
+    "url": "/react-portfolio/static/media/kindpng_2158189.a82f274d.png"
+  },
+  {
+    "revision": "177007f0cfaced65770d05c016697b4e",
+    "url": "/react-portfolio/static/media/universityLogo.177007f0.png"
+  },
+  {
+    "revision": "8fb0e5fd721a02afda190255b5ee649d",
+    "url": "/react-portfolio/static/media/project-cover10.8fb0e5fd.png"
+  },
+  {
+    "revision": "2c5e04ea42b6f824f1170910b456bc9c",
+    "url": "/react-portfolio/static/media/personalPhoto.2c5e04ea.jpg"
+  },
+  {
+    "revision": "3332cb46db18a8391f11ad1fb4ddbabc",
+    "url": "/react-portfolio/index.html"
+  }
+];
